af9ee686c811d2a64fd6a36f72be50725a934bb2
Tim <bendbeck@yahoo.com>
2021-10-27 20:13:36 +0300

    edit "WeIrD_StRiNg_CaSe.go", add work task solution

--- WeIrD StRiNg CaSe/WeIrD_StRiNg_CaSe.go
+++ WeIrD StRiNg CaSe/WeIrD_StRiNg_CaSe.go
@@ -1,5 +1,25 @@
 package main
 
-func toWeirdCase(str string) string {
-	return "SoMe StRiNg"	
+import "strings"
+
+func toWeirdCase(text string) string {
+	var wiredCaseText string
+	for _, word := range strings.Split(text, " "){
+		if wiredCaseText != ""{
+			wiredCaseText += " "
+		}
+		var wiredCaseWord string
+		for index, digitCode := range word{
+			digit := string(digitCode)
+			if index % 2 == 0{
+				digit = strings.ToUpper(digit)
+			} else {
+				digit = strings.ToLower(digit)
+			}
+			wiredCaseWord += digit
+		}
+		wiredCaseText += wiredCaseWord
+	}
+
+	return wiredCaseText
 }
