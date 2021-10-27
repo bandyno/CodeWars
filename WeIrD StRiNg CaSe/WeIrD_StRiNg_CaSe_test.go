9329edb1693340dd4e8d7b0f31a360ffcaa7d588
Tim <bendbeck@yahoo.com>
2021-10-27 20:12:46 +0300

    add tests, and correct old test
    old test was incorrect, was corrected, and was add three new tests

--- WeIrD StRiNg CaSe/WeIrD_StRiNg_CaSe_test.go
+++ WeIrD StRiNg CaSe/WeIrD_StRiNg_CaSe_test.go
@@ -4,16 +4,59 @@ import (
 	"testing"
 )
 
-func TestToWeirdCase(t *testing.T) {
+func TestToWeirdCaseOneWordUpperCase(t *testing.T) {
 	// arrange
-	text := "SoMe StRiNg"
+	text := "SOME"
+	wiredCaseText := "SoMe"
 
 	// act
-	result := toWeirdCase("some string")
+	result := toWeirdCase(text)
 
 	// assert
-	if result != text {
-		t.Errorf("Case was incorrected, got: %v, want: %v", text, result)
+	if result != wiredCaseText {
+		t.Errorf("Case was incorrected, got: %v, want: %v", text, wiredCaseText)
+
+	}
+}
+func TestToWeirdCaseOneWordLowerCase(t *testing.T) {
+	// arrange
+	text := "some"
+	wiredCaseText := "SoMe"
+
+	// act
+	result := toWeirdCase(text)
+
+	// assert
+	if result != wiredCaseText {
+		t.Errorf("Case was incorrected, got: %v, want: %v", text, wiredCaseText)
+
+	}
+}
+func TestToWeirdCaseTwoWordsLowerCase(t *testing.T) {
+	// arrange
+	text := "some string"
+	wiredCaseText := "SoMe StRiNg"
+
+	// act
+	result := toWeirdCase(text)
+
+	// assert
+	if result != wiredCaseText {
+		t.Errorf("Case was incorrected, got: %v, want: %v", result, wiredCaseText)
+
+	}
+}
+func TestToWeirdCaseTwoWordsUpperCase(t *testing.T) {
+	// arrange
+	text := "SOME STRING"
+	wiredCaseText := "SoMe StRiNg"
+
+	// act
+	result := toWeirdCase(text)
+
+	// assert
+	if result != wiredCaseText {
+		t.Errorf("Case was incorrected, got: %v, want: %v", result, wiredCaseText)
 
 	}
 }
