3e422c8970c665ecb49c2444d7b0c6368ee1da5e
Tim <bendbeck@yahoo.com>
2021-11-03 20:43:01 +0300

    edit: add test 'TestDecodeOneDigit'

--- RomanNumeralsDecoder/decoder_test.go
+++ RomanNumeralsDecoder/decoder_test.go
@@ -1,1 +1,19 @@
 package RomanNumeralsDecoder
+
+import (
+	"testing"
+)
+
+func TestDecodeOneDigit(t *testing.T){
+	// Arrange
+	romanNumber := "I"
+	decimalNumber := 1
+
+	// Act
+	result := Decode(romanNumber)
+
+	// Assert
+	if result != decimalNumber {
+		t.Errorf("func result was incorrect, got: %v, want: %v", result, decimalNumber)
+	}
+}
