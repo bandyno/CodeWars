2386ba385ec13a189e7643c5a229342ab2fd19d1
Tim <bendbeck@yahoo.com>
2021-11-03 21:11:54 +0300

    edit: add new variant of test 'TestDecodeFewDigits'

--- RomanNumeralsDecoder/decoder.go
+++ RomanNumeralsDecoder/decoder.go
@@ -1,5 +1,15 @@
 package RomanNumeralsDecoder
 
 func Decode(roman string) int {
-	return 0
+	dict :=  make(map[string]int)
+	dict["I"] = 1
+	dict["V"] = 5
+	dict["X"] = 10
+
+	result := 0
+	for _, v := range roman{
+		result += dict[string(v)]
+	}
+
+	return result
 }
--- RomanNumeralsDecoder/decoder_test.go
+++ RomanNumeralsDecoder/decoder_test.go
@@ -21,11 +21,19 @@ func TestDecodeFewDigits(t *testing.T)  {
 	// arrange
 	romanNumber := "XVII"
 	decimalNumber := 17
+
+	romanNumber1 := "XIV"
+	decimalNumber1 := 14
 	// act
 	result := Decode(romanNumber)
+	result1 := Decode(romanNumber1)
 	// assert
 	if decimalNumber != result{
 		t.Errorf("func result was incorrect, got: %v, want: %v", result, decimalNumber)
+	} 
+
+	if decimalNumber1 != result1{
+		t.Errorf("func result was incorrect, got: %v, want: %v", result1, decimalNumber1)
 	}
 
 }
