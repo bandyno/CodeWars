d2e038e667f20efe3e518e0ef8bd2397fa9dc5b1
Tim <bendbeck@yahoo.com>
2021-11-03 20:54:00 +0300

    edit: add test 'TestDecodeFewDigits'

--- RomanNumeralsDecoder/decoder_test.go
+++ RomanNumeralsDecoder/decoder_test.go
@@ -17,3 +17,15 @@ func TestDecodeOneDigit(t *testing.T){
 		t.Errorf("func result was incorrect, got: %v, want: %v", result, decimalNumber)
 	}
 }
+func TestDecodeFewDigits(t *testing.T)  {
+	// arrange
+	romanNumber := "XVII"
+	decimalNumber := 17
+	// act
+	result := Decode(romanNumber)
+	// assert
+	if decimalNumber != result{
+		t.Errorf("func result was incorrect, got: %v, want: %v", result, decimalNumber)
+	}
+
+}
