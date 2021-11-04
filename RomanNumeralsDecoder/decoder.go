370ef3f6b30438e0107c5b21f826e22d09afb70d
Tim <bendbeck@yahoo.com>
2021-11-04 10:33:51 +0300

    edit: was added task solution

--- RomanNumeralsDecoder/decoder.go
+++ RomanNumeralsDecoder/decoder.go
@@ -1,14 +1,23 @@
 package RomanNumeralsDecoder
 
 func Decode(roman string) int {
-	dict :=  make(map[string]int)
-	dict["I"] = 1
-	dict["V"] = 5
-	dict["X"] = 10
+	dict := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 	result := 0
-	for _, v := range roman{
-		result += dict[string(v)]
+	lastNum := 0
+	for _, v := range roman {
+		number := dict[string(v)]
+		if result == 0 {
+			result += number
+			lastNum = number
+		} else {
+			if number > lastNum {
+				result += number - lastNum*2
+			} else {
+				result += number
+			}
+		}
+		lastNum = number
 	}
 
 	return result
